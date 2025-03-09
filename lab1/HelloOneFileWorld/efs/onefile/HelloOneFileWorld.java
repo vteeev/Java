6208836144d77fcb6ae3236afd0c7567170fdfb8
vteeev <piotr.nie00@gmail.com>
2025-03-09 23:08:30 +0100

    lab1

---
+++ lab1/HelloOneFileWorld/efs/onefile/HelloOneFileWorld.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package efs.onefile;
+
+public class HelloOneFileWorld {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!\n");
+
+    }
+}
---
+++ lab1/HelloWorld/efs/manyfiles/UsedByHelloWorld.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package efs.manyfiles;
+
+public class UsedByHelloWorld {
+    public static void print() {
+
+        System.out.printf("Hello World on many files.");
+
+    }
+}
